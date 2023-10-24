995e645e119270b5bc630651fac0ee396ac9afb0
BensonTerra <benson.terra@hotmail.com>
2023-10-24 12:35:16 +0100

    ficha 5

---
+++ vueFicha5-events/src/components/Button2.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-on:click="showMessage">
+        <button v-on:click.once="showMessage">CLICA-ME(Normal)</button>
+        <button v-on:keyup.tab="showMessage">CLICA-ME(Tab)</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        showMessage() {
+            alert("Parabens!!! Você Clicou ou apertou Tab")
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
