0afd09929ebb1e4b520b49b727a5f01f5bc64a53
BensonTerra <benson.terra@hotmail.com>
2023-11-07 14:41:27 +0000

    save

--- vueFicha9-vueAnimals/src/App.vue
+++ vueFicha9-vueAnimals/src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-
+import headerVue from './components/header.vue'
 </script>
 
 <template>
   <v-app>
-    <header>
-      <v-main class=""></v-main>
-    </header>
+    <headerVue>
+      <v-main class="d-flex align-center justify-center">
+        <router-view/>
+      </v-main>
+    </headerVue>
   </v-app>
-
 
   <RouterView />
 </template>
---
+++ vueFicha9-vueAnimals/src/components/header.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-app-bar title="VueAnimals">
+        <router-link :to="{name: 'home'}" class="pr-3">Home</router-link>
+        <router-link :to="{name: 'about'}" class="pr-3">About</router-link>
+        <router-link to="/" class="pr-3">Catalog</router-link>
+        <router-link to="/" class="pr-3">Login</router-link>
+    </v-app-bar>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router'
+
+export default {}
+</script>
+
+<style>
+
+</style>
