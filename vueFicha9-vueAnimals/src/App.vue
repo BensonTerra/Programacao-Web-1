eb088238a3720c493981304efdce1dc52e12f368
BensonTerra <benson.terra@hotmail.com>
2023-11-07 11:53:50 +0000

    sace

---
+++ vueFicha9-vueAnimals/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <v-app>
+    <header>
+      <v-main class=""></v-main>
+    </header>
+  </v-app>
+
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
