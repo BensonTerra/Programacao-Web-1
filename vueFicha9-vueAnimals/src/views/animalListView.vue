5b2dfb0c9b0ca4d9709403c4cdea02489fad9539
BensonTerra <benson.terra@hotmail.com>
2023-11-08 23:51:02 +0000

    dsa

--- vueFicha9-vueAnimals/src/views/animalListView.vue
+++ vueFicha9-vueAnimals/src/views/animalListView.vue
@@ -1,6 +1,57 @@
 <template>
-  <v-card class="mx-auto" max-width="344">
+
+<v-card class="mx-auto" >
+
+    <v-container fluid>
+      <v-row dense>
+        <v-col v-for="card in cards" :key="card" :cols="card.flex">
+          <v-card>
+            <v-img
+              :src="card.src"
+              class="align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="400px" width="auto"
+              cover
+            >
+              <v-card-title class="text-white">{{ card.title }}</v-card-title>
+            </v-img>
+
+            <!--
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn
+                size="small"
+                color="surface-variant"
+                variant="text"
+                icon="mdi-heart"
+              ></v-btn>
+
+              <v-btn
+                size="small"
+                color="surface-variant"
+                variant="text"
+                icon="mdi-bookmark"
+              ></v-btn>
+
+              <v-btn
+                size="small"
+                color="surface-variant"
+                variant="text"
+                icon="mdi-share-variant"
+              ></v-btn>
+            </v-card-actions>
+          -->
+
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
     
+  </v-card>
+
+  <!--
+  <v-card class="mx-auto px-5" col = "4">
     <v-img
       src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
       height="200px"
@@ -36,13 +87,114 @@
       </div>
     </v-expand-transition>
   </v-card>
+
+  <v-card class="mx-auto px-5" col = "4">
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="200px"
+      cover
+    ></v-img>
+
+    <v-card-title> Top western road trips </v-card-title>
+
+    <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn color="orange-lighten-2" variant="text"> Explore </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+        @click="show = !show"
+      ></v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          I'm a thing. But, like most politicians, he promised more than he
+          could deliver. You won't have time for sleeping, soldier, not with all
+          the bed making you'll be doing. Then we'll go with that data file!
+          Hey, you add a one and two zeros to that or we walk! You're going to
+          do his laundry? I've got to find a way to escape.
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+
+  <v-card class="mx-auto px-5" col = "4">
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="200px"
+      cover
+    ></v-img>
+
+    <v-card-title> Top western road trips </v-card-title>
+
+    <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn color="orange-lighten-2" variant="text"> Explore </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+        @click="show = !show"
+      ></v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          I'm a thing. But, like most politicians, he promised more than he
+          could deliver. You won't have time for sleeping, soldier, not with all
+          the bed making you'll be doing. Then we'll go with that data file!
+          Hey, you add a one and two zeros to that or we walk! You're going to
+          do his laundry? I've got to find a way to escape.
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+  -->
+
+  
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+    data: () => ({
+      cards: [
+        {
+          title: 'Pre-fab homes',
+          src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg',
+          flex: 4,
+        },
+        {
+          title: 'Favorite road trips',
+          src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+          flex: 4,
+        },
+        {
+          title: 'Best airlines',
+          src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
+          flex: 4,
+        },
+        {
+          title: 'Best airlines',
+          src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
+          flex: 4,
+        },
+      ],
+    }),
+  }
 </script>
+
 
 <style>
 
