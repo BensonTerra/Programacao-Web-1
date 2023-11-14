dac6c70152ded7756b52312ec0804c1658ce6c8e
BensonTerra <benson.terra@hotmail.com>
2023-11-14 11:55:08 +0000

    SABE

--- vueFicha9-vueAnimals-Professor/src/main.js
+++ vueFicha9-vueAnimals-Professor/src/main.js
@@ -1,7 +1,11 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-// Vuetify
+
+
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
@@ -12,4 +16,8 @@ const vuetify = createVuetify({
   directives,
 })
 
-createApp(App).use(vuetify).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(vuetify)
+app.mount('#app')
---
+++ vueIntroPinia/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+  import { useCounterStore} from "@/stores/counter"
+  export default {
+    data() {
+      return {
+        store: useCounterStore()
+      }
+    },
+  }
+</script>
+
+<template>
+  <div>
+    <button @click="store.increment()">Click Me</button>
+    <h1>{{ store.count }}</h1>
+    <h1>{{ store.doubleCount }}</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
