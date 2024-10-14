7a4a44d37757631b7842677e5256e99abac2ffb0
BensonTerra <benson.terra@hotmail.com>
2024-10-14 15:35:58 +0100

    qw3ertyui

---
+++ Exercicio_complementar/vueComplementar_3/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import dynamicSize from "./components/dynamicFontSize.vue";
+</script>
+
+<template>
+  <dynamicSize/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Exercicio_complementar/vueComplementar_3/src/components/dynamicFontSize.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-bind:style="{ fontSize: size + 'px' }">
+    <p>Ola mundo maravilhoso</p>
+  </div>
+  <div>
+    <button v-bind:disabled="size == 5" @click="changeSize('-')">-</button>
+    <button v-bind:disabled="size == 15" @click="changeSize('+')">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      size: 10
+    };
+  },
+  methods: {
+    changeSize(dir) {
+      if (dir == "+") {this.size+=1}
+      else if (dir == "-") {this.size-=1}
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
